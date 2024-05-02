7a9af73c08ede7db1f51a663091017d963bb566e
minsub <hyohyosubsub@naver.com>
2024-05-02 17:33:46 +0900

    Algorithm Study Init

---
+++ src/Lv0/SequenceSectionQuery2.java
@@ -0,0 +1,29 @@
+package Lv0;
+
+public class SequenceSectionQuery2 {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+
+
+
+        for(int i = 0; i< queries.length; i++){
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+
+            // 32비트로 표현할 수 있는 최대 정수
+
+            int min = Integer.MAX_VALUE;
+            for(int j = s; j <=e; j++){
+                if(arr[j] > k && arr[j] < min){
+                    min = arr[j];
+                }
+            }
+            answer[i] = min == Integer.MAX_VALUE ? -1 : min;
+        }
+
+
+
+        return answer;
+    }
+}
---
+++ src/Lv0/SequenceSectionQuery3.java
@@ -0,0 +1,22 @@
+package Lv0;
+
+public class SequenceSectionQuery3 {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        // 입출력 예를 참고하면, queries 2차원 배열의 각 차원에 속한 원소 값들이 arr 배열에서의 위치를 의미하는 것을 확인할 수 있다.
+        // 즉 queries에 나온 위치 값에 따라 arr의 i와 j 위치의 값을 변경하는 것이다.
+
+        int temp = 0;
+
+        for(int i = 0; i < queries.length; i++){
+            temp = arr[queries[i][0]];
+            arr[queries[i][0]] = arr[queries[i][1]];
+            arr[queries[i][1]] = temp;
+        }
+
+        answer = arr;
+
+
+        return answer;
+    }
+}
