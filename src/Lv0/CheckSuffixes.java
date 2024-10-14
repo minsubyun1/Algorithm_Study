59e8532fbdebcd858583b57d9951c101143761cb
minsub <hyohyosubsub@naver.com>
2024-10-14 16:31:19 +0900

    -a

---
+++ src/Lv0/CheckSuffixes.java
@@ -0,0 +1,19 @@
+package Lv0;
+
+public class CheckSuffixes {
+    public int solution(String my_string, String is_suffix) {
+        int answer = 0;
+        String[] string_list = new String[my_string.length()];
+        for (int i = 0; i < my_string.length(); i++){
+            string_list[i] = my_string.substring(i, my_string.length());
+        }
+
+        for (int j = 0; j < string_list.length; j++){
+            if(string_list[j].equals(is_suffix)){
+                answer = 1;
+            }
+        }
+
+        return answer;
+    }
+}
---
+++ src/Lv0/MakingArray5.java
@@ -0,0 +1,26 @@
+package Lv0;
+import java.util.*;
+public class MakingArray5 {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+
+        ArrayList<Integer> answer_list = new ArrayList<>();
+
+
+        for(int i = 0; i < intStrs.length; i++){
+            int extracted_num = Integer.parseInt(intStrs[i].substring(s, l+s));
+            if(extracted_num > k){
+                answer_list.add(extracted_num);
+            }
+
+        }
+
+        int[] answer = new int[answer_list.size()];
+
+        for(int j = 0; j < answer.length; j++){
+            answer[j] = answer_list.get(j);
+        }
+
+
+        return answer;
+    }
+}
