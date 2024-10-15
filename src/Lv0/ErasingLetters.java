8f36d7082623034d327c14351ab5de8ea452d30c
minsub <hyohyosubsub@naver.com>
2024-10-15 17:20:05 +0900

    특정 인덱스에 속한 문자들로 문자만들기

---
+++ src/Lv0/ErasingLetters.java
@@ -0,0 +1,11 @@
+package Lv0;
+
+public class ErasingLetters {
+    public String solution(String my_string, int[] indices) {
+        char[] answer = my_string.toCharArray();
+        for(int i = 0; i < indices.length; i++){
+            answer[indices[i]] = ' ';
+        }
+        return new String(answer).replaceAll(" ","");
+    }
+}
