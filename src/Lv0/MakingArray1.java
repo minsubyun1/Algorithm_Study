5a57db2e18825a6f5ac421c9b74f6f6c669a6a21
minsub <hyohyosubsub@naver.com>
2024-10-15 17:14:09 +0900

    k배수 오름차순 배열 저장 알고리즘

---
+++ src/Lv0/MakingArray1.java
@@ -0,0 +1,17 @@
+package Lv0;
+
+public class MakingArray1 {
+    public int[] solution(int n, int k) {
+
+        int count = n / k;
+
+        int[] answer = new int[count];
+
+        for(int i = 1; i <= count; i++){
+            answer[i-1] = k * i;
+        }
+
+
+        return answer;
+    }
+}
