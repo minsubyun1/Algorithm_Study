45dadce9e17d5edc92f8fe7e9542edd1e5bcd98a
minsub <hyohyosubsub@naver.com>
2024-10-22 16:21:23 +0900

    숫자 쌍의 곱 최대값 구하기 음수포함

---
+++ src/Lv0/FindingMaxValue2.java
@@ -0,0 +1,20 @@
+package Lv0;
+import java.util.*;
+public class FindingMaxValue2 {
+    public int solution(int[] numbers) {
+        int answer = -100000000;
+
+
+        for(int i = 0; i < numbers.length; i++){
+            for(int j = numbers.length - 1; j > i; j--){
+                if(answer < numbers[i] * numbers[j]){
+                    answer = numbers[i] * numbers[j];
+                }
+            }
+        }
+
+
+
+        return answer;
+    }
+}
