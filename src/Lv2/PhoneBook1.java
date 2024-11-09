006a9b3222f193425ee871380dd9f0c2529a74c6
minsub <hyohyosubsub@naver.com>
2024-11-09 19:24:58 +0900

    문자열 정렬 후, 접두어 비교

---
+++ src/Lv2/PhoneBook1.java
@@ -0,0 +1,23 @@
+package Lv2;
+
+import java.util.*;
+
+public class PhoneBook1 {
+    class Solution {
+        public boolean solution(String[] phone_book) {
+            boolean answer = true;
+
+            Arrays.sort(phone_book);
+
+            for(int i = 0; i < phone_book.length-1; i++){
+                if(phone_book[i+1].length() >= phone_book[i].length()){
+                    if(phone_book[i].equals(phone_book[i+1].substring(0, phone_book[i].length()))){
+                        return false;
+                    }
+                }
+            }
+
+            return answer;
+        }
+    }
+}
