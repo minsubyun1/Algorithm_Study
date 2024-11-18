1c58c0bd1521a1c4247614a1c2ed50e1e475975e
minsub <hyohyosubsub@naver.com>
2024-11-18 15:55:35 +0900

    백준 브론즈1

---
+++ src/BOJ/Bronze/Divisor.java
@@ -0,0 +1,20 @@
+package BOJ.Bronze;
+
+import java.util.Arrays;
+import java.util.Scanner;
+public class Divisor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for(int i = 0; i < N; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[0] * arr[N-1]);
+    }
+}
