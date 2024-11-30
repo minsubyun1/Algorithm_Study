35124088f14fdd834c2d31f3640c121ae810ed43
minsub <hyohyosubsub@naver.com>
2024-11-30 23:00:59 +0900

    백준 브론즈3

--- src/BOJ/Bronze/ThreeNum.java
+++ src/BOJ/Bronze/ThreeNum.java
@@ -11,22 +11,9 @@ public class ThreeNum {
         for(int i = 0; i < arr.length; i++){
             arr[i] = sc.nextInt();
         }
-        Arrays.sort(arr);
+        Arrays.sort(arr); // 배열 정렬
 
-        int fir = arr[0];
-        int sec = 0;
-        int index = 0;
-
-        for(int i = 0; i < arr.length; i++){
-            if(fir <= arr[i]){
-                fir = arr[i];
-                index = i;
-            }
-        }
-
-        sec = arr[index-1];
-
-        System.out.println(sec);
+        System.out.println(arr[1]);
 
     }
 }
