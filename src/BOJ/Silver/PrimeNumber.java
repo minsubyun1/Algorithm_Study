ce1c949290f64327899f645b2e08159136e1682a
minsub <hyohyosubsub@naver.com>
2024-12-15 18:11:17 +0900

    백준 실버3(에라토스테네스의 체)

---
+++ src/BOJ/Silver/PrimeNumber.java
@@ -0,0 +1,32 @@
+package BOJ.Silver;
+
+import java.util.Scanner;
+public class PrimeNumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        int[] arr = new int[N + 1];
+        for (int i = 2; i <= N; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            if(arr[i]==0){
+                continue;
+            }
+            for(int j = i+i; j <= N; j = j + i){
+                arr[j] = 0;
+            }
+
+        }
+
+        for (int i = M;  i <= N; i++){
+            if(arr[i] != 0){
+                System.out.println(arr[i]);
+            }
+        }
+
+    }
+}
