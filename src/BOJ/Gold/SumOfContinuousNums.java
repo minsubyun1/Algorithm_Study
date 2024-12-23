6402614e21195f53f6491950e5fe6ee930ba24e9
minsub <hyohyosubsub@naver.com>
2024-12-23 17:18:45 +0900

    백준 골드 5

--- src/BOJ/Gold/SumOfContinuousNums.java
+++ src/BOJ/Gold/SumOfContinuousNums.java
@@ -1,37 +1,38 @@
-package BOJ.Silver;
+package BOJ.Gold;
 
 
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class pratice{
-    public static void main(String[] args) throws NumberFormatException, IOException{
+public class SumOfContinuousNums {
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
+        int []A = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++){
+        for(int i = 0; i < N; i++){
             A[i] = Integer.parseInt(st.nextToken());
         }
+
         int[] L = new int[N];
         L[0] = A[0];
         int result = L[0];
-        for (int i = 1; i < N; i++){
+        for(int i = 1; i < N; i++){
             L[i] = Math.max(A[i], L[i-1] + A[i]);
-            result = Math.max(result, L[i]); // 연속된 수를 뽑는데 중간에 식별
+            result = Math.max(result, L[i]); // 연속된 수를 뽑는데 중간에 삭제를 안하는 경우
         }
 
-        int[]R = new int[N];
-        R[N - 1] = A[N - 1];
-        for (int i = N - 2; i >= 0; i--){
+        int[] R = new int[N];
+        R[N-1] = A[N-1];
+        for(int i = N - 2; i > 0; i--){
             R[i] = Math.max(A[i], R[i+1] + A[i]);
         }
 
-        // 마나를 뺼 수 있음
-        for(int i = 1; i < N - 1; i++){
-            int temp = L[i - 1] + R[i + 1]; // 수열에서 i번째 수를 하나빼고 만들 수 있음
+        // 하나를 뺄 수 있음
+        for (int i = 1; i < N - 1; i++){
+            int temp = L[i-1] + R[i+1]; // 수열에서 i번째 수를 하나빼고 만들 수 있는 max
             result = Math.max(result, temp);
         }
 
