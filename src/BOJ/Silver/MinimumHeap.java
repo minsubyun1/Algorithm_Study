492a7502296493e6e344298f9c2c79418291975d
minsub <hyohyosubsub@naver.com>
2025-02-12 01:08:53 +0900

    백준 실버2(최소힙-우선순위 큐)

---
+++ src/BOJ/Silver/MinimumHeap.java
@@ -0,0 +1,32 @@
+package BOJ.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class MinimumHeap {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(N);
+
+        for (int i = 0; i < N; i++){
+            int order = Integer.parseInt(br.readLine());
+            if (order == 0){
+                if(queue.isEmpty()){
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(queue.poll()).append("\n");
+                }
+            } else {
+                queue.add(order);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
