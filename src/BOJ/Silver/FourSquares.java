62ea5b8537c88d803b38f80ac94132461a70ee87
minsub <hyohyosubsub@naver.com>
2025-02-12 23:55:55 +0900

    백준 실버3(최소힙-우선순위 큐)

---
+++ src/BOJ/Silver/FourSquares.java
@@ -0,0 +1,24 @@
+package BOJ.Silver;
+
+import java.util.Scanner;
+public class FourSquares {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+
+        int[] D = new int[N + 1];
+        D[0] = 0;
+        D[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            D[i] = i; // 최악의 경우 (1^2 + 1^2 + --- + 1^2)로 i개 필요
+            for (int j = 1; j * j <= i ; j++) { // j는 가능한 제곱수 (1, 4, 9, ...)
+                D[i] = Math.min(D[i], D[i - j * j] + 1);
+            }
+        }
+
+        System.out.println(D[N]); // 최소 개수 출력
+
+    }
+}
