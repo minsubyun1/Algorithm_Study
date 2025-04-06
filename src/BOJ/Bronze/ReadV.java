82f92689e6f45d556f3238461c76333bee2d5fde
minsub <hyohyosubsub@naver.com>
2025-04-06 23:16:38 +0900

    백준 브론즈1(이차원 배열)

--- src/BOJ/Bronze/ReadV.java
+++ src/BOJ/Bronze/ReadV.java
@@ -1,6 +1,6 @@
 package BOJ.Bronze;
 
-import java.util.Scanner;
+import java.util.*;
 public class ReadV {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
