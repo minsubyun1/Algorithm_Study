38d4c48cf6bcccba7e18be3d762ff68e8ecb443e
minsub <hyohyosubsub@naver.com>
2025-05-12 00:12:48 +0900

    백준 실버1(DP)

--- src/BOJ/Silver/IntegerTri.java
+++ src/BOJ/Silver/IntegerTri.java
@@ -20,7 +20,7 @@ public class IntegerTri {
         // 맨 꼭대기 초기화
         dp[0][0] = triangle[0][0];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i < N; i++) {
             for (int j = 0; j <= i; j++) {
                 if (j == 0) {
                     dp[i][j] = dp[i - 1][j] + triangle[i][j];
